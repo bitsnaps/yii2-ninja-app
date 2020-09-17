f5a71de5c02e757f3cd53ffcef5b31ee0184a59d
Ibrahim.H <bitsnaps@yahoo.fr>
2020-09-17 21:24:12 +0100

    first commit

---
+++ console/config/main.php
@@ -0,0 +1,31 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/params.php')
+);
+
+$config = [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'console\controllers',
+    'bootstrap' => ['log'],
+    'components' => [
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
